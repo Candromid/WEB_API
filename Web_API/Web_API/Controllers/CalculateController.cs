a6581afa04573679c153d68508f1bbfa90a00150
Nurbek Nurdaulet <mainaccnurbek@mail.ru>
2023-09-23 12:40:02 +0500

    added controllers

---
+++ Web_API/Web_API/Controllers/CalculateController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API.Models;
+
+namespace Web_API.Controllers
+{
+    [Route("api/calculate")]
+    public class CalculateController : Controller
+    {
+        private readonly CalculateServices _calculateServices;
+
+        public CalculateController(CalculateServices calculateServices)
+        {
+            _calculateServices = calculateServices;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Calculate(
+            [FromBody] CalculateRequest request)
+        {
+            var calculateResponse = await _calculateServices.Calculate(request);
+
+            return Ok(calculateResponse);
+        }
+    }
+}
