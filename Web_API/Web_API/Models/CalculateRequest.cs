b38d9031fea3aa14bd3e5221ef83de40516c3e63
Nurbek Nurdaulet <mainaccnurbek@mail.ru>
2023-09-23 12:23:23 +0500

    Added models in the project

---
+++ Web_API/Web_API/Models/CalculateRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Web_API.Models
+{
+    public class CalculateRequest
+    {
+        public int Weight { get; set; }
+        public int[] Dimensions { get; set; }
+        public string FromCityCode { get; set; }
+        public string ToCityCode { get; set;}
+        public bool Production { get; set; }
+
+    }
+}
---
+++ Web_API/Web_API/Models/CalculateResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_API.Models
+{
+    public class CalculateResponse
+    {
+        public string ServiceType { get; set; }
+        public double TotalCost { get; set; }
+    }
+}
